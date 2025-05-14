acba21aa4979ee4416545b10c2b75f793ad90950
gydeng <gydeng@stu.pku.edu.cn>
2025-05-14 19:52:39 +0800

    Add modules and the test trial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import _ from 'lodash';
+import {initJsPsych} from 'jspsych';
+import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+
+const jsPsych = initJsPsych();
+const trial = {
+  type: htmlKeyboardResponse,
+  stimulus: `<div style="font-weight: bold;">Hello world!</div>`,
+};
+jsPsych.run([trial]);
